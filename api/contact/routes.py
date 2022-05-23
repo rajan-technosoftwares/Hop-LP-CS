29a2bcf9967eb5244ba599bcf5c03f7c2d418057
Rajan <rajan.b.technosoftwares@gmail.com>
2022-05-23 12:42:32 +0530

    routes.py updated

--- api/contact/routes.py
+++ api/contact/routes.py
@@ -14,7 +14,7 @@ def contact():
         email=request.form.get('email')
         address=request.form.get('address')
         phone_num=request.form.get('phone_num')
-        print(name,email,address,phone_num)
+        # print(name,email,address,phone_num)
 
         entry=Contacts(name=name,email=email,address=address,phone_num=phone_num)
         db.session.add(entry)
