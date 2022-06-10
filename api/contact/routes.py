6d901d97a901522c0bff64299df8669cc8e21732
Rajan <rajan.b.technosoftwares@gmail.com>
2022-06-10 13:21:39 +0530

    routes.py updated

--- api/contact/routes.py
+++ api/contact/routes.py
@@ -10,14 +10,50 @@ contact_api=Blueprint('contact_api',__name__,url_prefix='/')
 def contact():
 
     if request.method=='POST':
-        name=request.form.get('name')
-        email=request.form.get('email')
-        address=request.form.get('address')
-        phone_num=request.form.get('phone_num')
+        
+        data=request.form
         
 
-        entry=Contact(name=name,email=email,address=address,phone_num=phone_num)
+        entry=Contact(name=data['name'],email=data['email'],address=data['address'],phone_num=data['phone_num'])
         db.session.add(entry)
         db.session.commit()
 
-    return render_template('add copy.html')
+    
+    return "contact form submitted"
+
+# @contact_api.route('/show_data')
+# def show_data():
+    
+#     data= db.session.query(Contact).all()
+#     d=[]
+#     for i in data:
+#         d.append({
+#             'name': i.name,
+#             'email': i.email,
+#             'address': i.address,
+#             'phone_num': i.phone_num,
+#         })
+#     return {'data': d}
+
+
+# @contact_api.route('update_data')
+# def update_data():
+#     data = Contact.query.filter_by(id=7).one()
+    
+#     if data :
+#         data.name='Ravi'
+#         db.session.add(data)
+#         db.session.commit()
+
+    
+    
+#     return {'message': 'updated'}
+
+# @contact_api.route('/delete',methods=['POST','GET'])
+# def delete():
+#     if request.method== 'POST':
+#         id_number=request.form.get('id')
+#         data= Contact.query.filter(Contact.id==id_number).one()
+#         db.session.delete(data)
+#         db.session.commit()
+#     return render_template('del.html')
