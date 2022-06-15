11d826fe0fd7d820ca058eb1bce467d91c70ee20
Rajan <rajan.b.technosoftwares@gmail.com>
2022-06-15 14:18:30 +0530

    code updated

--- api/contact/models.py
+++ api/contact/models.py
@@ -8,5 +8,5 @@ class Contact(db.Model):
     id=db.Column(db.Integer,nullable=False ,primary_key=True)
     name=db.Column(db.String(50))
     email=db.Column(db.String(50))
-    pin_code=db.Column(db.Integer)
+    address=db.Column(db.String(100))
     phone_num=db.Column(db.Integer)
--- api/contact/routes.py
+++ api/contact/routes.py
@@ -6,7 +6,7 @@ from api.contact.models import Contact
 
 contact_api=Blueprint('contact_api',__name__,url_prefix='/')
 
-@contact_api.route('/contact-us', methods=['POST'])
+@contact_api.route('/contact-us', methods=['GET','POST'])
 def contact():
 
     if request.method=='POST':
@@ -14,9 +14,46 @@ def contact():
         data=request.form
         
 
-        entry=Contact(name=data['name'],email=data['email'],pin_code=data['pin_code'],phone_num=data['phone_num'])
+        entry=Contact(name=data['name'],email=data['email'],address=data['address'],phone_num=data['phone_num'])
         db.session.add(entry)
         db.session.commit()
 
     
     return "contact form submitted"
+
+# @contact_api.route('/show_data')
+# def show_data():
+    
+#     data= db.session.query(Contact).all()
+#     d=[]
+#     for i in data:
+#         d.append({
+#             'name': i.name,
+#             'email': i.email,
+#             'address': i.address,
+#             'phone_num': i.phone_num,
+#         })
+#     return {'data': d}
+
+
+# @contact_api.route('update_data')
+# def update_data():
+#     data = Contact.query.filter_by(id=7).one()
+    
+#     if data :
+#         data.name='Ravi'
+#         db.session.add(data)
+#         db.session.commit()
+
+    
+    
+#     return {'message': 'updated'}
+
+# @contact_api.route('/delete',methods=['POST','GET'])
+# def delete():
+#     if request.method== 'POST':
+#         id_number=request.form.get('id')
+#         data= Contact.query.filter(Contact.id==id_number).one()
+#         db.session.delete(data)
+#         db.session.commit()
+#     return render_template('del.html')
