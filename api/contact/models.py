3923d0ab9d9267fb1508e6b0a792910e4be52e55
Rajan <rajan.b.technosoftwares@gmail.com>
2022-06-15 14:04:00 +0530

    code updated

--- api/contact/models.py
+++ api/contact/models.py
@@ -8,5 +8,5 @@ class Contact(db.Model):
     id=db.Column(db.Integer,nullable=False ,primary_key=True)
     name=db.Column(db.String(50))
     email=db.Column(db.String(50))
-    address=db.Column(db.String(100))
+    pin_code=db.Column(db.Integer)
     phone_num=db.Column(db.Integer)
