fc1979b028950d345eb6fd2c777afc040ee53ae1
Rajan <rajan.b.technosoftwares@gmail.com>
2022-06-15 16:28:15 +0530

    code updated

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 db=SQLAlchemy()
 
@@ -8,6 +9,8 @@ db_name= 'contacts.db'
 
 def create_app():
     app=Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
     
     app.config.from_object('config.Config')
     db.init_app(app)
--- api/contact/routes.py
+++ api/contact/routes.py
@@ -2,19 +2,18 @@ from flask import Blueprint
 from flask import request,render_template
 from api import db
 from api.contact.models import Contact
-
+from flask_cors import cross_origin
 
 contact_api=Blueprint('contact_api',__name__,url_prefix='/')
 
 @contact_api.route('/contact-us', methods=['POST'])
+@cross_origin()
 def contact():
 
     if request.method=='POST':
         
         data=request.form
-        data.headers.add('Access-Control-Allow-Origin','*')
         
-
         entry=Contact(name=data['name'],email=data['email'],pin_code=data['pin_code'],phone_num=data['phone_num'])
         db.session.add(entry)
         db.session.commit()
