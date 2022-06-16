2423c9bb7c5ba293c724437dcee76b87d5bfc6ce
Rajan <rajan.b.technosoftwares@gmail.com>
2022-06-16 13:05:14 +0530

    code updated

--- api/contact/routes.py
+++ api/contact/routes.py
@@ -9,27 +9,27 @@ from flask_cors import cross_origin
 
 contact_api=Blueprint('contact_api',__name__,url_prefix='/')
 
-@contact_api.route('/contact-us', methods=['POST'])
+@contact_api.route('/contact-us', methods=['GET','POST'])
 @cross_origin()
 def contact():
 
-    if request.method=='POST':
+    # if request.method=='POST':
         
-        d=request.get_data()
-        data=json.loads(d)
-        print(data)
-        # resp = Response("Contact Form Submitted")
-        # resp.headers['Access-Control-Allow-Origin'] = '*'
-        entry=Contact(name=data['name'],email=data['email'],pin_code=data['pin_code'],phone_num=data['phone_num'])
-        db.session.add(entry)
-        db.session.commit()
-                     
-        msg = Message(
-                'Contact Details',
-                recipients = ['bagoriarajan@gmail.com']
-               )
-        msg.body = json.dumps(data)
-        print(mail.send(msg))
+    d=request.get_data()
+    data=json.loads(d)
+    print(data)
+    # resp = Response("Contact Form Submitted")
+    # resp.headers['Access-Control-Allow-Origin'] = '*'
+    entry=Contact(name=data['name'],email=data['email'],pin_code=data['pin_code'],phone_num=data['phone_num'])
+    db.session.add(entry)
+    db.session.commit()
+                    
+    msg = Message(
+            'Contact Details',
+            recipients = ['bagoriarajan@gmail.com']
+            )
+    msg.body = json.dumps(data)
+    print(mail.send(msg))
         
         
 
